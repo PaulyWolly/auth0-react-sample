085fad2ef271e4f1403f9697cc30b7c0675c19ea
Paul Welby <pwelby@gmail.com>
2022-02-12 13:59:16 -0800

    updated content for login config for Auth0

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -3,10 +3,11 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
+
   const history = useHistory();
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+  const domain = "pwelby.auth0.com"
+  const clientId = "pZFNBRRA6a9A5GDBiYaBWPqn12OYl0ym"
+  const audience = "http://Auth0-React.sample"
 
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);

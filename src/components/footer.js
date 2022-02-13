809191172f68fad64f96085f7e3db9e4c6db3387
Paul Welby <pwelby@gmail.com>
2022-02-13 06:10:14 -0800

    footer change to styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,15 +2,17 @@ import React from "react";
 import './footer.css'
 
 const Footer = () => (
-  <footer className="footer p-2 text-center">
-    <div className="logo" />
-    <p className="footer-text">
-      Auth by {" "}
-      <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
-        Auth0
-      </a>
-    </p>
-  </footer>
+  <div className="footer-container">
+    <footer className="footer p-2 text-center">
+      <div className="logo" />
+      <p className="footer-text">
+        Auth by {" "}
+        <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
+          Auth0
+        </a>
+      </p>
+    </footer>
+  </div>
 );
 
 export default Footer;

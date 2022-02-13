9b2b6ed1cdbb6d0e13cc7920971c7d6d2524d970
Paul Welby <pwelby@gmail.com>
2022-02-12 22:04:29 -0800

    footer stylings

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => (
   <footer className="footer p-2 text-center">
     <div className="logo" />
-    <p>
+    <p className="footer-text">
       Auth by {" "}
       <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
         Auth0
--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,53 +1,55 @@
 import {NavLink} from "react-router-dom";
 import React from "react";
+import './main-nav.css'
 
 const MainNav = () => (
   <div className="navbar-nav mr-auto">
-    <NavLink
-      to="/"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Home
-    </NavLink>
+    <div className="nav-links">
+      <NavLink
+        to="/"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Home
+      </NavLink>
 
-    {/* <NavLink
-      to="/profile"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Profile
-    </NavLink> */}
+      {/* <NavLink
+        to="/profile"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Profile
+      </NavLink> */}
 
-    <NavLink
-      to="/external-api"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      External API
-    </NavLink>
+      <NavLink
+        to="/external-api"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        External API
+      </NavLink>
 
-    <NavLink
-      to="/about"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      About
-    </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        About
+      </NavLink>
 
-    <NavLink
-      to="/todos"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      API Call
-    </NavLink>
-
+      <NavLink
+        to="/todos"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        API Call
+      </NavLink>
+    </div>
   </div>
 );
 
